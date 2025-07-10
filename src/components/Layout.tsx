bc03aa28aff8642d2c097e33baf734638df0fa96
igorgcampos <igor.gonsalves@gmail.com>
2025-07-10 09:38:57 -0300

    changes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -30,8 +30,10 @@ export function Layout({ children }: LayoutProps) {
 
       <div className="flex min-h-screen w-full">
         <AppSidebar />
-        <main className="flex-1 overflow-auto">
-          {children}
+        <main className="flex-1 overflow-auto flex justify-center">
+          <div className="w-full max-w-7xl mx-auto p-6">
+            {children}
+          </div>
         </main>
       </div>
     </SidebarProvider>
